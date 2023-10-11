162df2d2926e585ff14407d1992391125b55f826
AnhMTV <maithachvietanh@gmail.com>
2023-10-11 23:59:33 +0700

    Add data with images

--- scripts/fetch-data.mjs
+++ scripts/fetch-data.mjs
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import axios from "axios";
 import path from "path";
+
 const MAP_CONTENT_TYPE = {
     'chain': 'chains',
     'category': 'categories',
@@ -12,68 +13,70 @@ const savePath = (contentType) => `data/${contentType}/list.json`;
 const saveImagesPath = (contentType) => `data/${contentType}/images`;
 const urlImage = (folder, field, name) => `https://static-data.subwallet.app/${folder}/images/${field}/${name}`;
 
-export async function downloadFile (url, downloadDir, forceFileName = null) {
-  // Create dir if not exist
-  if (!fs.existsSync(downloadDir)) {
-    fs.mkdirSync(downloadDir, {recursive: true});
-  }
+export async function downloadFile(url, downloadDir, forceFileName = null) {
+    // Create dir if not exist
+    if (!fs.existsSync(downloadDir)) {
+        fs.mkdirSync(downloadDir, {recursive: true});
+    }
 
-  // Download file with axios
-  let fileName = url.split('/').pop();
-  if (forceFileName) {
-    fileName = forceFileName + '.' + fileName.split('.').pop();
-  }
-  const filePath = path.join(downloadDir, fileName);
+    // Download file with axios
+    let fileName = url.split('/').pop();
+    if (forceFileName) {
+        fileName = forceFileName + '.' + fileName.split('.').pop();
+    }
+    const filePath = path.join(downloadDir, fileName);
 
-  // Download and save file
-  const writer = fs.createWriteStream(filePath);
+    // Download and save file
+    const writer = fs.createWriteStream(filePath);
 
-  const response = await axios({
-    url,
-    method: 'GET',
-    responseType: 'stream'
-  });
-
-  response.data.pipe(writer);
-
-  return await new Promise((resolve, reject) => {
-    writer.on('finish', () => {
-      resolve(fileName);
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
     });
-    writer.on('error', reject);
-  });
+
+    response.data.pipe(writer);
+
+    return await new Promise((resolve, reject) => {
+        writer.on('finish', () => {
+            resolve(fileName);
+        });
+        writer.on('error', reject);
+    });
 }
 
 async function writeJSONFile(filePath, data) {
-  fs.writeFile(filePath, JSON.stringify(data, null, 2), function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("JSON saved to " + filePath);
-    }
-})
-}
-const main = async () => {
-    const key = process.argv[2];
-    const results = await axios.get(urlParams(key));
-    if (!results.data) return;
-    const folder = MAP_CONTENT_TYPE[key] || key;
-    const path = savePath(folder);
-    const fieldsImage = ['icon', 'logo', 'backdrop_image'];
-    const downloadDir = saveImagesPath(folder);
-    const dataContent = await Promise.all(results.data.map(async item => {
-        const dataImages = {};
-        for (const field of fieldsImage) {
-            const dataField = item[field];
-            if (dataField) {
-                const folderFieldImage = `${downloadDir}/${field}`;
-                const newFileName = await downloadFile(dataField, folderFieldImage);
-                dataImages[field] = urlImage(folder, field, newFileName);
-            }
+    fs.writeFile(filePath, JSON.stringify(data, null, 2), function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("JSON saved to " + filePath);
         }
-        return {...item, ...dataImages};
-    }));
-    await writeJSONFile(path, dataContent);
+    })
+}
+
+const main = async () => {
+    for (const key in MAP_CONTENT_TYPE) {
+        const results = await axios.get(urlParams(key));
+        if (!results.data) return;
+        const folder = MAP_CONTENT_TYPE[key] || key;
+        const path = savePath(folder);
+        const fieldsImage = ['icon', 'logo', 'backdrop_image'];
+        const downloadDir = saveImagesPath(folder);
+        const dataContent = await Promise.all(results.data.map(async item => {
+            const dataImages = {};
+            for (const field of fieldsImage) {
+                const dataField = item[field];
+                if (dataField) {
+                    const folderFieldImage = `${downloadDir}/${field}`;
+                    const newFileName = await downloadFile(dataField, folderFieldImage);
+                    dataImages[field] = urlImage(folder, field, newFileName);
+                }
+            }
+            return {...item, ...dataImages};
+        }));
+        await writeJSONFile(path, dataContent);
+    }
 }
 
 main().catch((error) => console.error(error));
