6624245f113b9951e8e32b4cf6106a07468a0093
AnhMTV <maithachvietanh@gmail.com>
2023-10-18 16:49:27 +0700

    Update period-time.json

--- scripts/generate-period-time.mjs
+++ scripts/generate-period-time.mjs
@@ -3,8 +3,8 @@ import {writeJSONFile} from "./utils.mjs";
 const periodMs = 12 * 7 * 24 * 60 * 60 * 1000;
 const periodKMs = 6 * 7 * 24 * 60 * 60 * 1000;
 const RELAY_DATE_MAP = {
-  polkadot: (new Date('2021-12-21T01:48:00')).getTime() - (6 * periodMs),
-  kusama: (new Date('2021-09-08T04:27:00')).getTime() - (15 * periodKMs),
+  polkadot: (new Date('2021-12-21T01:59:00')).getTime() - (6 * periodMs),
+  kusama: (new Date('2021-09-08T04:46:00')).getTime() - (15 * periodKMs),
 }
 
 function computeTime(relayChain, period) {
